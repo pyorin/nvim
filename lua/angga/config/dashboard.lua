f81f73fe5a5dd93c49a809796a23e62b87b963ef
Herlangga Maulani <maulani.morningstar@gmail.com>
2023-01-05 09:29:48 +0700

    add screenshot

---
+++ lua/angga/config/dashboard.lua
@@ -0,0 +1,68 @@
+local db = require("dashboard")
+
+local header = {
+	[[⡿⠋⠄⣀⣀⣤⣴⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⣌⠻⣿⣿]],
+	[[⣴⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⠹⣿]],
+	[[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠹]],
+	[[⣿⣿⡟⢹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡛⢿⣿⣿⣿⣮⠛⣿⣿⣿⣿⣿⣿⡆]],
+	[[⡟⢻⡇⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣣⠄⡀⢬⣭⣻⣷⡌⢿⣿⣿⣿⣿⣿]],
+	[[⠃⣸⡀⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠈⣆⢹⣿⣿⣿⡈⢿⣿⣿⣿⣿]],
+	[[⠄⢻⡇⠄⢛⣛⣻⣿⣿⣿⣿⣿⣿⣿⣿⡆⠹⣿⣆⠸⣆⠙⠛⠛⠃⠘⣿⣿⣿⣿]],
+	[[⠄⠸⣡⠄⡈⣿⣿⣿⣿⣿⣿⣿⣿⠿⠟⠁⣠⣉⣤⣴⣿⣿⠿⠿⠿⡇⢸⣿⣿⣿]],
+	[[⠄⡄⢿⣆⠰⡘⢿⣿⠿⢛⣉⣥⣴⣶⣿⣿⣿⣿⣻⠟⣉⣤⣶⣶⣾⣿⡄⣿⡿⢸]],
+	[[⠄⢰⠸⣿⠄⢳⣠⣤⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⣼⣿⣿⣿⣿⣿⣿⡇⢻⡇⢸]],
+	[[⢷⡈⢣⣡⣶⠿⠟⠛⠓⣚⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣇⢸⠇⠘]],
+	[[⡀⣌⠄⠻⣧⣴⣾⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠛⠛⠛⢿⣿⣿⣿⣿⣿⡟⠘⠄⠄]],
+	[[⣷⡘⣷⡀⠘⣿⣿⣿⣿⣿⣿⣿⣿⡋⢀⣠⣤⣶⣶⣾⡆⣿⣿⣿⠟⠁⠄⠄⠄⠄]],
+	[[⣿⣷⡘⣿⡀⢻⣿⣿⣿⣿⣿⣿⣿⣧⠸⣿⣿⣿⣿⣿⣷⡿⠟⠉⠄⠄⠄⠄⡄⢀]],
+	[[⣿⣿⣷⡈⢷⡀⠙⠛⠻⠿⠿⠿⠿⠿⠷⠾⠿⠟⣛⣋⣥⣶⣄⠄⢀⣄⠹⣦⢹⣿]],
+	[[]],
+	[[]],
+	[[]],
+}
+
+db.custom_header = header
+db.custom_center = {
+	{
+		icon = "  ",
+		desc = "Find File                    ",
+		action = require("telescope.builtin").find_files,
+		shortcut = "SPC ff",
+	},
+	{
+		icon = "  ",
+		desc = "Browse File                  ",
+		action = ":Neotree toggle float",
+		shortcut = "SPC e",
+	},
+	{
+		icon = "  ",
+		desc = "New File                     ",
+		action = ":ene!",
+		shortcut = "SPC x",
+	},
+	{
+		icon = "  ",
+		desc = "Open Terminal                ",
+		action = ":ToggleTerm",
+		shortcut = "Ctrl T",
+	},
+	{
+		icon = "  ",
+		desc = "Configure                ",
+		action = ":e ~/.config/nvim/init.lua",
+		shortcut = "         ",
+	},
+	{
+		icon = "  ",
+		desc = "Exit Neovim                 ",
+		action = ":q!",
+		shortcut = "      ",
+	},
+}
+
+db.custom_footer = {
+	[[]],
+	[[]],
+	"https://github.com/pyorin/nvim.aing",
+}
